5bdd3e32025f425839ddc0d9b5e69087e2d0aa2f
D4C-lolu <dareolumideisfly@gmail.com>
2020-07-17 14:50:21 +0100

    Updates

---
+++ precache-manifest.0ed8c70d65471ef42dc899c59396ed31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41121a17c4e015a38e27407ddd7bfd93",
+    "url": "/Weather-app/index.html"
+  },
+  {
+    "revision": "43b55d38695c551443ff",
+    "url": "/Weather-app/static/css/main.a36f5895.chunk.css"
+  },
+  {
+    "revision": "5986386869d4c0fe5eb5",
+    "url": "/Weather-app/static/js/2.a570c07b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-app/static/js/2.a570c07b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43b55d38695c551443ff",
+    "url": "/Weather-app/static/js/main.e6fe1741.chunk.js"
+  },
+  {
+    "revision": "a2861103e0194e8c138c",
+    "url": "/Weather-app/static/js/runtime-main.1d012fff.js"
+  }
+]);
